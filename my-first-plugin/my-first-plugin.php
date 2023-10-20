c0d4bbb8222e938dc5e194621659d56c5efbff57
Tostyfis 360 <tostyfis360@gmail.com>
2023-10-20 17:20:17 +0200

    Limpieza en el directorio

--- my-first-plugin/my-first-plugin.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-Plugin Name: Divinity IA Assistant
-Description: Incorpora un chat, potenciado por chat GTP-4 para aconsejar y ayudar a un cliente
-*/
-
-// Include mfp-functions.php, use require_once to stop the script if mfp-functions.php is not found
-require_once plugin_dir_path(__FILE__) . 'includes/mfp-functions.php';
-?>
